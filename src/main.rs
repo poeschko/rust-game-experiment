2e099494f7240bea4cce6ae134126c3faf67d222
Jan Poeschko <jan@poeschko.com>
2018-04-29 18:57:36 +0100

    add a floor

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,31 @@ use ggez::graphics::{clear, circle, present, DrawMode, Point2};
 use ggez::timer;
 
 use na::{Vector2, Translation2};
-use ncollide::shape::{Ball};
+use ncollide::shape::{Ball, Plane};
 use nphysics2d::world::World;
-use nphysics2d::object::RigidBody;
+use nphysics2d::object::{RigidBody, RigidBodyHandle};
 
 struct MainState {
     world: World<f32>,
+    player: RigidBodyHandle<f32>,
 }
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let mut world = World::new();
         world.set_gravity(Vector2::new(0.0, 9.81));
-        // let floor = RigidBody::new_static(Plane::new(Vector2::new(-1.0, -1.0)), 0.3, 0.6);
+        let mut floor = RigidBody::new_static(Plane::new(Vector2::new(0.0, -1.0)), 0.3, 0.6);
+        floor.append_translation(&Translation2::new(0.0, 400.0));
+        world.add_rigid_body(floor);
         let mut player = RigidBody::new_dynamic(Ball::new(10.0), 1.0, 0.3, 0.6);
-        player.append_translation(&Translation2::new(30.0, 10.0));
-        world.add_rigid_body(player);
-        let s = MainState { world: world };
+        player.append_translation(&Translation2::new(30.0, 200.0));
+        let player = world.add_rigid_body(player);
+        let s = MainState { world, player };
         Ok(s)
     }
 }
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         const DESIRED_FPS: u32 = 60;
-
         while timer::check_update_time(ctx, DESIRED_FPS) {
             self.world.step(1.0 / DESIRED_FPS as f32);
         }
@@ -40,10 +42,8 @@ impl EventHandler for MainState {
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         clear(ctx);
-        for rb in self.world.rigid_bodies() {
-            let pos = rb.borrow().position_center();
-            circle(ctx, DrawMode::Fill, Point2::new(pos.x, pos.y), 10.0, 0.5)?;
-        }
+        let pos = self.player.borrow().position_center();
+        circle(ctx, DrawMode::Fill, Point2::new(pos.x, pos.y), 10.0, 0.5)?;
         present(ctx);
         Ok(())
     }
